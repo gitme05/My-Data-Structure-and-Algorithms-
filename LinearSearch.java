044ce9fb89d6e7a4a67c66b996bf95c08def237d
gitme05 <rhyandanielgimongala4@gmail.com>
2024-10-07 20:47:42 +0800

    Created LinearSearch.java

---
+++ LinearSearch.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class LinearSearch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter the number of elements: ");
+        int n = sc.nextInt();
+       
+        int[] array = new int[n];
+        System.out.println("Enter the elements of the array: ");
+        for (int i = 0; i < n; i++) {
+            array[i] = sc.nextInt();      
+        }
+       
+        System.out.print("Enter the value to search using Linear Search: ");
+        int target = sc.nextInt();
+        
+        int linearResult = linearSearch(array, target);
+        System.out.println("Linear Search result: " + (linearResult != -1 ? "Found at index " + linearResult : "Not found"));
+    }
+    
+    public static int linearSearch(int[] array, int target) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
